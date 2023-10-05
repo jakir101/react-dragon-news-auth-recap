7ec900c6f7492bad38dd133a2355ede2814db3e7
jakir101 <mdjakir729742@gmail.com>
2023-10-05 11:39:41 +0600

    added login component

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h2 className="text-2xl">Please Login</h2>
+        </div>
+    );
+};
+
+export default Login;
